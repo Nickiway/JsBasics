92d4308c12daab65be9b8b6313d5ae005256e3e0
nickiway <nickiwayua@gmail.com>
2023-01-28 18:27:57 +0100

    Classes folder creating

--- Codewars/Dou/Никита Шкитак.js
+++ Codewars/Dou/Никита Шкитак.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// case 1
 const salaries1 = {
     Manager: { salary: 1000, tax: "10%" },
     Designer: { salary: 600, tax: "30%" },
@@ -14,7 +13,6 @@ const team1 = [
     { name: "Leo", specialization: "Artist"},
 ];
 
-// case 2
 const salaries2 = { 
     TeamLead : {salary : 1000, tax : "99%"},
     Architect : {salary : 9000, tax : "34%"},
@@ -28,13 +26,11 @@ const team2 = [
     {name : "Leo", specialization : "Artist"}
 ];
 
-// count a non-taxed payment
 let countWithoutTax = (salary, tax) => Math.round(salary * 100 / (100 - +(tax.split("%")[0])));
 
 const financeReport1 = calculateTeamFinanceReport(salaries1, team1);
 const financeReport2 = calculateTeamFinanceReport(salaries2, team2);
 
-// main func
 function calculateTeamFinanceReport(salaries, team) {
     let withTaxPayments = new Map();
     let specializations = Object.keys(salaries);
@@ -42,16 +38,13 @@ function calculateTeamFinanceReport(salaries, team) {
         totalBudgetTeam : 0,
     };
 
-    // getting an array of specializations
     for (let specialization of specializations) {
         withTaxPayments.set(specialization, 
         countWithoutTax(salaries[specialization].salary, salaries[specialization].tax));
 
-        // setting each spec value = 0
         output[`totalBudget${specialization}`] = 0;
     }
 
-    // checking each teamp partic.
     for (let member of team) {
         if (specializations.includes(member.specialization)) {
             output[`totalBudget${member.specialization}`] += withTaxPayments.get(member.specialization);
